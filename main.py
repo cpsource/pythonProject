c6f9770581338b8cf34df08d9d7b01a5bb7a2764
cpsource <page.cal@gmail.com>
2023-05-29 06:55:26 -0400

    first test

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 # This is a sample Python script.
 import numpy as np
+from matplotlib import pyplot as plt
+
+from sklearn.datasets import make_checkerboard
+from sklearn.cluster import SpectralBiclustering
+from sklearn.metrics import consensus_score
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+n_clusters = (4, 3)
+data, rows, columns = make_checkerboard(
+    shape=(300, 300), n_clusters=n_clusters, noise=10, shuffle=False, random_state=0
+    )
+
+plt.matshow(data, cmap=plt.cm.Blues)
+plt.title("Original dataset")
+
+# for test, exit here
+plt.show()
+exit(0)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
