d792d9ff303703da92fea3ce9396c7934b429316
cpsource <page.cal@gmail.com>
2023-05-30 01:09:44 -0400

    these three config lines were added by PyTorch

--- torch-test.py
+++ torch-test.py
@@ -4,3 +4,11 @@ x = torch.rand(5, 3)
 print(x)
 
 print("Torch is available: ", torch.cuda.is_available())
+
+#
+# Note: This was in front of the run
+#
+#/home/pagec/scraper/venv/bin/python /home/pagec/.local/share/JetBrains/Toolbox/apps/PyCharm-P/ch-0/231.9011.38/plugins/python/helpers/pydev/pydevconsole.py --mode=client --host=127.0.0.1 --port=43609
+#import sys; print('Python %s on %s' % (sys.version, sys.platform))
+#sys.path.extend(['/home/pagec/PycharmProjects/pythonProject'])
+#
